b4380394ed04eaf29890b37e083c870e429cfeaa
maheenwaris <codeherway9@gmail.com>
2024-09-02 21:31:13 +0500

    hugo version added

---
+++ in-hugo/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "#b4d3b2",
+      tgreen: '#45B096',
+      twhite: '#ffff',
+      slate500: '#2D3748', // Slate-500 color
+    },
+  },
+  plugins: [],
+}
